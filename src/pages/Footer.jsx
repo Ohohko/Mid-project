aa7eb9e359c06bfed427562381d80ecffda3f8ce
O_had <ohadkachlon@gmail.com>
2023-03-04 22:17:36 +0200

    fix footer

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -121,9 +121,9 @@ function Footer() {
           <input type="text" id="footer-field" name="footer-field" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:bg-transparent focus:ring-yellow-200 focus:border-yellow-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
         </div>
         <button className="inline-flex text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded">Button</button>
-        {/* <p className="text-gray-500 text-sm md:ml-6 md:mt-0 mt-2 sm:text-left text-center">Bitters chicharrones fanny pack
-          <br className="lg:block hidden">waistcoat green juice
-        </p> */}
+        <p className="text-gray-500 text-sm md:ml-6 md:mt-0 mt-2 sm:text-left text-center">Bitters chicharrones fanny pack
+          <br className="lg:block hidden"/>waistcoat green juice
+        </p>
       </div>
       <span className="inline-flex lg:ml-auto lg:mt-0 mt-6 w-full justify-center md:justify-start md:w-auto">
         <a className="text-gray-500">
