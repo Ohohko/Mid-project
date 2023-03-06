02fbf4e55c560a3d6e98b689fda742199c5c7222
O_had <ohadkachlon@gmail.com>
2023-03-06 13:10:48 +0200

    2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { TypeAnimation } from "react-type-animation";
-import {TfiShoppingCart} from "react-icons/tfi"
+import { TfiShoppingCart } from "react-icons/tfi";
 
 function Navbar() {
   return (
@@ -14,11 +14,32 @@ function Navbar() {
           repeat={Infinity}
           style={{ fontSize: "2em" }}
         />
-        <div>
-          <form>
-            <input></input>
-          </form>
+        <div className="form-control">
+          <div className="input-group">
+            <input
+              type="text"
+              placeholder="Search…"
+              className="input input-bordered"
+            />
+            <button className="btn btn-square">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
+
         <div>
           <ul className="flex items-center space-x-10 text-sm">
             <li>
@@ -45,9 +66,8 @@ function Navbar() {
               </Link>
             </li>
           </ul>
-          
         </div>
-        <TfiShoppingCart/>
+        <TfiShoppingCart />
       </nav>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,16 @@ function Home() {
   return (
     <html data-theme="dark">
   <div data-theme="light">
+  <div className="hero min-h-screen" style={{ backgroundImage: `url("https://woocommerce-955837-3332579.cloudwaysapps.com/wp-content/uploads/2023/03/gift1.jpeg")` }}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
 <ProductList/>
   </div>
 </html>
