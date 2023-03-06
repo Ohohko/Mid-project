8caef9738d1f419dc10fe085b3621927ad19abeb
O_had <ohadkachlon@gmail.com>
2023-03-06 11:21:20 +0200

    1

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,18 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const url = 'https://woocommerce-955837-3332579.cloudwaysapps.com/wp-json/wc/v3/products';
-const consumerKey = 'ck_3a68611a79bba9b558ba55d203f3740538c33c05';
-const consumerSecret = 'cs_59912f1b38ec272e63ebf2f7224b6eab08c93b60';
+function ProductList() {
+  const [products, setProducts] = useState([]);
 
-axios.get(url, {
-  auth: {
-    username: consumerKey,
-    password: consumerSecret,
-  }
-})
-  .then(response => console.log(response.data))
-  .catch(error => console.error(error));
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+    const url = 'https://woocommerce-955837-3332579.cloudwaysapps.com/wp-json/wc/v3/products';
+    const consumerKey = 'ck_3a68611a79bba9b558ba55d203f3740538c33c05';
+    const consumerSecret = 'cs_59912f1b38ec272e63ebf2f7224b6eab08c93b60';
+    try {
+      const response = await axios.get(url, {
+        auth: {
+          username: consumerKey,
+          password: consumerSecret,
+        },
+      });
+      setProducts(response.data);
+      console.log(response)
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  fetchProducts();
+}, []);
+
+
+return (
+  <div className="product-list">
+    {products.map(product => (
+      <div className="product-card" key={product.id}>
+        <img src={product.images[0].src} alt={product.name} />
+        <h2>{product.name}</h2>
+        <p>{product.price}</p>
+        <button>Add to cart</button>
+      </div>
+    ))}
+  </div>
+);
+}
 
 
 export default ProductList;
